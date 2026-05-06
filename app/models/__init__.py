b13e39f61562eabf905cf75da12b34740c63a6ad
DucPham1512 <duc.phamtan@hcmut.edu.vn>
2026-05-06 15:10:50 +0700

    Add ChatMessage and WebhookEvent models; update Stream model for Mux integration

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,3 +4,7 @@ from app.models.stream import Stream  # noqa: F401
 from app.models.gesture import GestureMapping  # noqa: F401
 from app.models.user import User  # noqa: F401
 from app.models.media import MediaItem  # noqa: F401
+from app.models.webhook_event import WebhookEvent
+from app.models.chat_message import ChatMessage
+
+__all__ = ["Stream", "GestureMapping", "WebhookEvent", "ChatMessage"]
